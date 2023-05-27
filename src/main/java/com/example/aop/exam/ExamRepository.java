1d673145e3406a23f5d450aa066cfd3229219977
minu <made_power@naver.com>
2023-05-27 15:01:13 +0900

    Aop 활용
    배웠던 개념을 토대로 간단하게 로그를 추적하는 기능을 추가했다.

---
+++ src/main/java/com/example/aop/exam/ExamRepository.java
@@ -0,0 +1,21 @@
+package com.example.aop.exam;
+
+import com.example.aop.exam.annotation.Trace;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExamRepository {
+    private static int seq=0;
+
+            /*
+             5번 중 한번 실패하는 요청
+             */
+    @Trace
+    public String save(String itemId){
+        seq++;
+        if (seq % 5==0){
+            throw new IllegalStateException("예외 발생!");
+        }
+        return "ok";
+    }
+}
