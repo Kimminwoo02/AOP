2455dae31d6f79d6a3ba9f37e9c687f9a82ca93a
minu <made_power@naver.com>
2023-05-27 14:07:24 +0900

    Bean
    Bean 지시자 에 관하여 학습

---
+++ src/test/java/com/example/aop/pointcut/BeanTest.java
@@ -0,0 +1,32 @@
+package com.example.aop.pointcut;
+
+import com.example.aop.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class BeanTest {
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    void success(){
+        orderService.orderItem("itemA");
+    }
+
+    @Aspect
+    static class BeanAspect {
+        @Around("bean(orderService) || bean (*Repository)")
+        public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable{
+            log.info("[bean] {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+
+    }
+}
